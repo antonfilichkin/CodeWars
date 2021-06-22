323845c3e39763cc95b47374af3884f780438c8f
Anton Filichkin <a.filichkin@mail.ru>
2021-06-22 17:24:14 +0300

    AreTheyTheSame

--- src/test/java/AreTheyTheSame/AreSameTest.java
+++ src/test/java/AreTheyTheSame/AreSameTest.java
@@ -2,7 +2,8 @@ package AreTheyTheSame;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class AreSameTest {
 
@@ -12,18 +13,18 @@ class AreSameTest {
                 new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361}));
 
         assertTrue(AreSame.comp(new int[]{-1, -5, 10},
-                                new int[]{100, 25, 1}));
+                new int[]{100, 25, 1}));
     }
 
     @Test
     void testAreSameReturnFalse() {
-        assertFalse(AreSame.comp(new int[] {120, 144, 19, 161, 19, 144, 19, 11},
-                new int[] {121, 14641, 20736, 361, 25921, 361, 20736, 361}));
+        assertFalse(AreSame.comp(new int[]{120, 144, 19, 161, 19, 144, 19, 11},
+                new int[]{121, 14641, 20736, 361, 25921, 361, 20736, 361}));
 
-        assertFalse(AreSame.comp(new int[] {}, new int[] {361, 20736, 361}));
+        assertFalse(AreSame.comp(new int[]{}, new int[]{361, 20736, 361}));
 
         int[] a;
         a = null;
-        assertFalse(AreSame.comp(a, new int[] {21, 14641, 20736, 361, 25921, 361, 20736, 361}));
+        assertFalse(AreSame.comp(a, new int[]{21, 14641, 20736, 361, 25921, 361, 20736, 361}));
     }
 }
